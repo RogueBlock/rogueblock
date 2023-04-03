48a1b3af8875e7646443328fdaf453112f063b52
Altynbek Orumbayev <aorumbayev@protonmail.com>
2023-04-03 16:55:30 +0200

    chore: adding new report by d13

--- common/frauds.ts
+++ common/frauds.ts
@@ -7,5 +7,9 @@ export const fraudWallets: FraudWallet[] = [
   {
     address: 'MVEKYHFLJ63UKDYGNKCJD7WO5KFJZFVFMJPSDAWLDIDP4LUP575YDOW6GI',
     label: 'MyAlgo Hacker Wallet #1'
+  },
+  {
+    address: 'NG7P2KCBIJBWK4YZE47F5XVYZDGCTZDYRJHKINFGFJKH6RATVFODV4LU2A',
+    label: 'MyAlgo Hacker Wallet #2'
   }
 ]
